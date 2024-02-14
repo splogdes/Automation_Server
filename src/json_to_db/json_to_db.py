f988c4817460b21b79f775cc9c3454a69cc7cc8a
splogdes <95136830+splogdes@users.noreply.github.com>
2024-02-14 00:58:15 +0000

    change indents and i need to fix timing some how

--- src/json_to_db/json_to_db.py
+++ src/json_to_db/json_to_db.py
@@ -2,43 +2,43 @@ from . import data_base as db
 import socket
 
 class json_to_db:
-    '''This class is used to process the data received from the UDP server and insert it into the database'''
+	'''This class is used to process the data received from the UDP server and insert it into the database'''
 
-    def __init__(self, sock: socket.socket, db_name = 'data.db'):
-        '''Initializes the class with the socket and the database name'''
-        self.sock = sock
-        self.__data_base = db.DataBase(db_name)
-
-
-    def is_device_registered(self, data):
-        '''Checks if the device is already registered in the database'''
-        return self.__data_base.is_device_registered(data['mac'])
-    
-    def register_device(self, data, addr):
-        '''Registers the device in the database'''
-        self.__data_base.insert_device(data['mac'], addr[0], addr[1], data['model'], data['location'])
-        print("Device registered")
-
-    def register_sensor(self, data, addr):
-        '''Registers the sensor in the database'''
-        if not self.__data_base.is_sensor_registered(data['mac'], data['name']):
-            self.__data_base.insert_sensor(data['mac'], data['name'], data['model'])
-            self.sock.sendto(("registerd sensor:" + data['name']).encode(), addr)
-
-    def update_device(self, data, addr):
-        '''Updates the device in the database'''
-        self.__data_base.update_device(data['mac'], addr[0], addr[1], data['model'], data['location'])
-        self.sock.sendto("device updated".encode(), addr)
-
-    def new_sensor_data(self, data, addr):
-        '''Inserts the sensor data into the database'''
-        
-        for sensor in data['sensors']:
-            if not self.__data_base.is_sensor_registered(data['mac'], sensor):
-                self.sock.sendto(sensor.encode(), addr)
-
-        self.__data_base.insert_message(data['mac'], data['sensors'])
-        self.sock.sendto(("sensor data received " + sensor).encode(), addr)
+	def __init__(self, sock: socket.socket, db_name = 'data.db'):
+		'''Initializes the class with the socket and the database name'''
+		self.sock = sock
+		self.__data_base = db.DataBase(db_name)
 
 
-    
+	def is_device_registered(self, data):
+		'''Checks if the device is already registered in the database'''
+		return self.__data_base.is_device_registered(data['mac'])
+	
+	def register_device(self, data, addr):
+		'''Registers the device in the database'''
+		self.__data_base.insert_device(data['mac'], addr[0], addr[1], data['model'], data['location'])
+		print("Device registered")
+
+	def register_sensor(self, data, addr):
+		'''Registers the sensor in the database'''
+		if not self.__data_base.is_sensor_registered(data['mac'], data['name']):
+			self.__data_base.insert_sensor(data['mac'], data['name'], data['model'])
+			self.sock.sendto(("registerd sensor:" + data['name']).encode(), addr)
+
+	def update_device(self, data, addr):
+		'''Updates the device in the database'''
+		self.__data_base.update_device(data['mac'], addr[0], addr[1], data['model'], data['location'])
+		self.sock.sendto("device updated".encode(), addr)
+
+	def new_sensor_data(self, data, addr):
+		'''Inserts the sensor data into the database'''
+		
+		for sensor in data['sensors']:
+			if not self.__data_base.is_sensor_registered(data['mac'], sensor):
+				self.sock.sendto(sensor.encode(), addr)
+
+		self.__data_base.insert_message(data['mac'], data['sensors'])
+		self.sock.sendto(("sensor data received " + sensor).encode(), addr)
+
+
+	
--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from udp_server import client_handler
+from json_to_db import DataBase
+import matplotlib.pyplot as plt
+from datetime import datetime
 
+def server():
+	udp_server = client_handler()
+	udp_server.run()
+ 
 def main():
-    
-    server = client_handler()
+    db = DataBase()
+    data = db.get_data('8C:AA:B5:15:F8:E8', 'room monitor', 'temperature')
 
-    server.run()
+    x = [datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S').strftime('%H:%M:%S') for x in data]
+    y = [x[0] for x in data]
+    print(x)
+    plt.plot(y)
+    plt.show()
+	
+	
 
 if __name__ == "__main__":
-    main()
+	main()
