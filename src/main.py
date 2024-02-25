640cf142c8d89d995995e3673d01448d558ca76f
splogdes <95136830+splogdes@users.noreply.github.com>
2024-02-25 19:56:11 +0000

    small fixes

--- src/main.py
+++ src/main.py
@@ -9,11 +9,19 @@ def server():
  
 def main():
     db = DataBase()
-    data = db.get_data('5C:CF:7F:02:59:0E', 'room monitor', 'temperature')
-    print(data)
-    x = [datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S') for x in data]
-    y = [x[0] for x in data]
-    plt.plot(x, y)
+    data1 = db.get_data('5C:CF:7F:02:59:0E', 'room monitor', 'temperature')
+    data2 = db.get_data('5C:CF:7F:02:59:0E', 'room monitor', 'humidity')
+    # data = db.get_data('5C:CF:7F:02:59:0E', 'gas sensor', 'air_quality')
+    # print(data)
+    x1 = [datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S') for x in data1]
+    y1 = [x[0] for x in data1]
+    x2 = [datetime.strptime(x[1], '%Y-%m-%d %H:%M:%S') for x in data2]
+    y2 = [x[0] for x in data2]
+    fig , ax = plt.subplots(2,1)
+    ax[0].plot(x1, y1)
+    ax[1].plot(x2, y2)
+    ax[0].set_title('Temperature')
+    ax[1].set_title('Humidity')
     plt.show()
 	
 	
