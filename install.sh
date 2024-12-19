ed7df8220ad98c4e4e461ced884e96e381c10bb2
splogdes <95136830+splogdes@users.noreply.github.com>
2024-12-19 22:43:32 +0000

    update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 WORK_DIR="$HOME/Automation_Server"
 
 # Update package list
-sudo apt get update
+sudo apt update
 
 # Install dependencies
 sudo apt install -y xorg openbox xauth python3 python3-pip python3-venv python-is-python3
@@ -17,11 +17,17 @@ python3 -m venv "$WORK_DIR/venv"
 sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
 
 # Autologin
-sudo sed -i 's/ExecStart=.*/ExecStart=-\/sbin\/agetty --autologin rpiserver --noclear %I $TERM/' /etc/systemd/system/getty@tty1.service
+sudo cp /etc/systemd/system/getty.target.wants/getty@tty1.service /etc/systemd/system/
+sudo sed -i 's/ExecStart=.*/ExecStart=-\/sbin\/agetty --autologin rpiserver --noclear - $TERM/' /etc/systemd/system/getty@tty1.service
 
 sudo systemctl daemon-reload
+sudo systemctl enable getty@tty1.service
 
 # Create service files
+chmod +x $WORK_DIR/service/service.sh
+
+mkdir -p $HOME/.config/systemd/user
+
 tee $HOME/.config/systemd/user/automation_server.service << 'EOF'
 [Unit]
 Description=Automation Server
